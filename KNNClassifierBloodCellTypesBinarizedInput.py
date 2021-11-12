b77870f49fa1dd54aaab676ba1b568fc09c0e946
ejh243 <e.j.hannon@exeter.ac.uk>
2021-11-12 09:49:04 +0000

    remove parameters to separate file

--- KNNClassifierBloodCellTypesBinarizedInput.py
+++ KNNClassifierBloodCellTypesBinarizedInput.py
@@ -7,33 +7,27 @@ import sys
 import os
 import pandas as pd
 import numpy as np
+from params import * # where params.py contains parameters
 from sklearn.neighbors import KNeighborsClassifier
 
-chr = int(sys.argv[1])
+chr = int(sys.argv[1]) 
 nobs = int(sys.argv[2]) # how many "single cell" observations to simulate per cell type
 
-os.chdir("/mnt/data1/Eilis/Projects/Asthma/ClassifyCellTypes/")
+os.chdir(workDir)
 
-pThres = 0.5 ## threshold to keep probes with signif diffs
-methStatus = (0,1) 
-max_window = 10000 # max distance between outer most cpgs in classifier
-min_cpg = 2 # number of cpgs in first classifer
-nCV = 15 # number of iterations to simulate cross validation
+chr = int(sys.argv[1]) ## provided from command line
 
+probeAnno = pd.read_csv(trainDataPath + "rowanno_chr" + str(chr) + ".csv").values
 
-
-probeAnno = pd.read_csv("TrainingData/rowanno_chr" + str(chr) + ".csv").values
+np.random.seed(randomSeed) ## ensures that the sample random sampling occurs across algorithms and across reruns of the script
 
 ## to speed up computation exclude sites with no evidence of cell type diffs from ANOVA
 ## equivalent to excluding features that don't vary
-probeAnno = probeAnno[probeAnno[:,5] < pThres,:]
-
-
+probeAnno = probeAnno[probeAnno[:,nCT] < pThres,:]
 
 ## sort by position
-probeAnno = probeAnno[np.argsort(probeAnno[:, 7]),:]
-
-ctProbs = probeAnno[:,0:5] ## matrix of probability of being methylated by cell type
+probeAnno = probeAnno[np.argsort(probeAnno[:, nCT+2]),:]
+ctProbs = probeAnno[:,0:nCT] ## matrix of probability of being methylated by cell type
 
 
 ## as sensitivity of array is poor at the extremes were meth level is estimated as >0.9 should be effectively 1, and <0.1, 0
@@ -65,7 +59,7 @@ Y = np.repeat(np.arange(0,nCT), nobs)
 nsites = np.shape(probeAnno)[0]
 
 ## create empty array to append results to
-results = np.empty((0,14))
+results = np.empty((0,4+(2*nCT))) 
 
 # for each site, iteratively add sites to predict until a max window size is hit
 site_index = 0
@@ -73,10 +67,10 @@ ncpg = min_cpg
 
 while site_index < nsites:
     print("Running site: " + str(site_index + 1) + " of " + str(nsites))
-    start = probeAnno[site_index,7]
+    start = probeAnno[site_index,nCT+2]
     ## need catch for if size of classifier is greater than number of sites left on chr
     if( site_index+ncpg <= nsites):
-        stop = probeAnno[(site_index+ncpg-1),7]
+        stop = probeAnno[(site_index+ncpg-1),nCT+2]
         windowSize = stop - start
     else:
         windowSize = max_window
@@ -89,13 +83,13 @@ while site_index < nsites:
             X = np.transpose(train_obs[site_index:(site_index+ncpg),:,l])
             model = KNeighborsClassifier(nCT)
             model.fit = model.fit(X,Y) 
-            test_pred = model.predict(np.transpose(test_obs[site_index:(site_index+ncpg),:]))
+            test_pred = model.predict(np.transpose(test_obs[site_index:(site_index+ncpg),:,l]))
             bool_correct = np.equal(test_pred, Y)
            ## count number correct for each cell type to caluclate sensitivity
             cvValues[l,:nCT] = np.array([sum(x) for x in np.split(bool_correct, nCT)], dtype = float)/nobs
             
             ## calculate specificity
-            cvValues[l,nCT:] = np.array([sum((test_pred != x) & (Y != x))/(4*nobs) for x in np.arange(5)], dtype = float)
+            cvValues[l,nCT:] = np.array([sum((test_pred != x) & (Y != x))/(nCT*nobs) for x in np.arange(5)], dtype = float)
             
             l += 1
         # summarise performance
@@ -110,4 +104,4 @@ while site_index < nsites:
         ncpg = min_cpg
 
 
-np.savetxt("Results/KNNBinarizedChr" + str(chr) + "_" + str(nobs) + "obsCT.csv", results, delimiter=",")
+np.savetxt(resultsPath + "KNNBinarizedChr" + str(chr) + "_" + str(nobs) + "obsCT.csv", results, delimiter=",")
--- NaiveBayesHierarchicalClassifierBinarizedInput.py
+++ NaiveBayesHierarchicalClassifierBinarizedInput.py
@@ -6,37 +6,33 @@ import sys
 import os
 import pandas as pd
 import numpy as np
+from params import * # where params.py contains parameters
 from sklearn.naive_bayes import BernoulliNB
 
 chr = int(sys.argv[1])
 nobs = int(sys.argv[2]) # how many "single cell" observations to simulate per cell type
 
-os.chdir("/mnt/data1/Eilis/Projects/Asthma/ClassifyCellTypes/")
+os.chdir(workDir)
 
-pThres = 0.5 ## threshold to keep probes with signif diffs
-methStatus = (0,1) 
-max_window = 10000 # max distance between outer most cpgs in classifier
-min_cpg = 2 # number of cpgs in first classifer
-nCV = 15 # number of iterations to simulate cross validation
+probeAnno = pd.read_csv(trainDataPath + "rowanno_chr" + str(chr) + ".csv").values
 
-
-probeAnno = pd.read_csv("TrainingData/rowanno_chr" + str(chr) + ".csv").values
+np.random.seed(randomSeed) ## ensures that the sample random sampling occurs across algorithms and across reruns of the script
 
 ## to speed up computation exclude sites with no evidence of cell type diffs from ANOVA
 ## equivalent to excluding features that don't vary
-probeAnno = probeAnno[probeAnno[:,5] < pThres,:]
+probeAnno = probeAnno[probeAnno[:,nCT] < pThres,:]
 
 ## sort by position
-probeAnno = probeAnno[np.argsort(probeAnno[:, 7]),:]
+probeAnno = probeAnno[np.argsort(probeAnno[:, nCT+2]),:]
+ctProbs = probeAnno[:,0:nCT] ## matrix of probability of being methylated by cell type
 
-ctProbs = probeAnno[:,0:5] ## matrix of probability of being methylated by cell type
 
 ## as sensitivity of array is poor at the extremes were meth level is estimated as >0.9 should be effectively 1, and <0.1, 0
 ## change these values
 ctProbs = np.where(ctProbs > 0.9, 1, ctProbs)
 ctProbs = np.where(ctProbs < 0.1, 0, ctProbs)
 
-## create binary test and training data used mean DNAm as probability a CpG is methylated for a particular read
+## create bin test and training data used mean DNAm as probability a CpG is methylated for a particular read
 nsites, nCT = np.shape(ctProbs)
 train_obs = np.empty((nsites, nCT*nobs, nCV), dtype = int)
 test_obs = np.empty((nsites, nCT*nobs, nCV), dtype = int)
@@ -57,20 +53,22 @@ while l < nCV:
 ## array of cell type labels (i.e. what we want to predict)
 Y = np.repeat(np.arange(0,nCT), nobs)
 
+nsites = np.shape(probeAnno)[0]
 
 ## create empty array to append results to
-results = np.empty((0,14))
+results = np.empty((0,4+(2*nCT))) 
 
 # for each site, iteratively add sites to predict until a max window size is hit
 site_index = 0
 ncpg = min_cpg
 
+
 while site_index < nsites:
     print("Running site: " + str(site_index + 1) + " of " + str(nsites))
-    start = probeAnno[site_index,7]
+    start = probeAnno[site_index,nCT+2]
     ## need catch for if size of classifier is greater than number of sites left on chr
     if( site_index+ncpg <= nsites):
-        stop = probeAnno[(site_index+ncpg-1),7]
+        stop = probeAnno[(site_index+ncpg-1),nCT+2]
         windowSize = stop - start
     else:
         windowSize = max_window
@@ -104,4 +102,4 @@ while site_index < nsites:
         ncpg = min_cpg
 
 
-np.savetxt("Results/NaiveBayesBernoulliBinarizedChr" + str(chr) + "_" + str(nobs) + "obsCT.csv", results, delimiter=",")
+np.savetxt(resultsPath + "NaiveBayesBernoulliBinarizedChr" + str(chr) + "_" + str(nobs) + "obsCT.csv", results, delimiter=",")
