49f842cfb2cce14de5d98e0e6f33242b7d4cd871
ejh243 <e.j.hannon@exeter.ac.uk>
2026-04-01 16:52:12 +0100

    add SLURM commands

--- jobSubmission/runONTTesting.sh
+++ jobSubmission/runONTTesting.sh
@@ -1,4 +1,14 @@
-
+#!/bin/sh
+#SBATCH --export=ALL # export all environment variables to the batch job.
+#SBATCH -p mrcq # submit to the serial queue
+#SBATCH --time=100:00:00 # Maximum wall time for the job.
+#SBATCH -A Research_Project-MRC190311 # research project to submit under. 
+#SBATCH --nodes=1 # specify number of nodes.
+#SBATCH --ntasks-per-node=16 # specify number of processors per node
+#SBATCH --mail-type=END # send email at job completion 
+#SBATCH --output=LogFiles/testONTData-%A.o
+#SBATCH --error=LogFiles/testONTData-%A.e
+#SBATCH --job-name=testONTData-%A.e
 
 
 ## loops through chromosomes and model types to select regions with good enough accuracy for testing on ONT data
