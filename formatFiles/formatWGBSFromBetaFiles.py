9a5f0e94cff1f302ad73a73d4dc71a74f0d5260b
ejh243 <e.j.hannon@exeter.ac.uk>
2026-04-01 09:41:36 +0100

    add logging messages

--- formatFiles/formatWGBSFromBetaFiles.py
+++ formatFiles/formatWGBSFromBetaFiles.py
@@ -1,7 +1,9 @@
-## extract data from beta files
+## extract data from beta files assumes in folder 1_raw in data_path
 ## filter based on min read depth
-## matrix of mean dnam level across cell types
-## anova of cell type
+## matrix of mean dnam level across cell types saved to 2_processed
+## output includes anova of cell type
+## command line arguments are <cpg_reference_path> <sample_sheet_path> <data_path> <chr>
+## sampleSheet format filename, group,cell type
 
 def processBeta(filename, minRD = 10):
     content = np.fromfile(filename, dtype=np.uint8).reshape((-1, 2))
@@ -12,7 +14,7 @@ def testCTDiffs(betas, X):
     model = sm.OLS(betas.to_numpy(), X).fit()
     return(model.f_test("x1 = x2 = x3 = x4").pvalue)
 
-
+import os
 import sys
 import numpy as np
 import pandas as pd
@@ -29,10 +31,20 @@ print("Processing chr " + str(chr))
 
 ## load CpG location info
 cpgLoci = pd.read_csv(cpgFile, sep = "\t", header = None)
+print("CpG reference file contains " + str(cpgLoci.shape[0]) + " sites")
 
 ## load blood samples
-sampleList = pd.read_csv(sampleSheet, sep = "\t", header = None)
-betaMat = pd.concat([processBeta(x,10) for x in sampleList[0]], axis = 1)
+sampleList = pd.read_csv(sampleSheet, header = None)
+print("Loading data for " + str(sampleList.shape[0]) + " samples")
+fileList = [resPath + "1_raw/" + x for x in sampleList[0]]
+betaMat = pd.concat([processBeta(x,10) for x in fileList], axis = 1)
+
+print("Beta matrix loaded for " + str(betaMat.shape[0]) + " sites")
+
+## check reference file matches beta matrix
+if  betaMat.shape[0] != cpgLoci.shape[0]:
+    print("Number of CpGs in beta file and CpG reference file do not match. Exiting.")
+    sys. exit(1)
 
 ## filter to chromosome sites
 betaMat = betaMat[cpgLoci[0].isin(["chr" + str(chr)])]
@@ -56,7 +68,13 @@ cpgLoci['P'] = betaMat.apply(lambda row : testCTDiffs(row, X), axis = 1)
 print("Saving files")
 ## remove "chr 
 cpgLoci["chr"] = [str(x).lstrip("chr") for x in cpgLoci[0]]
+
+## write files
+if not os.path.exists(resPath + "2_processed"):
+    os.makedirs(resPath + "2_processed")
   
-cpgLoci[["P","chr",1]].to_csv(resPath + "rowanno_chr" + str(chr) + ".csv", index=False)
+cpgLoci[["P","chr",1]].to_csv(resPath + "2_processed/rowanno_chr" + str(chr) + ".csv", index=False)
     
-betaMat.to_csv(resPath + "betas_chr" + str(chr) + ".csv", index=False)
+betaMat.to_csv(resPath + "2_processed/betas_chr" + str(chr) + ".csv", index=False)
+
+print("All data successfully writen to file.")
