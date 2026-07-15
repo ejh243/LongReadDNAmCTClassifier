872d97fbc21f0a744fd4c33de5452dfb1ed0f766
ejh243 <e.j.hannon@exeter.ac.uk>
2026-07-15 10:09:44 +0100

    iterate through multiple thresholds

--- summariseResults/writeRegionsToFile.py
+++ summariseResults/writeRegionsToFile.py
@@ -10,7 +10,8 @@ import pyranges as pr
 resultsPath = sys.argv[1]
 chr = sys.argv[2]
 nCT = sys.argv[3]
-threshold = 0.95
+minThreshold = 0.8 # lowest accuracy threshold to consider a model predictive
+thresholdInc = 0.05 # increment to increase threshold
 
 os.chdir(resultsPath)
 
@@ -21,8 +22,8 @@ chrFiles = list(filter(lambda x:'_Chr'+chr+'_' in x, allFiles))
 
 allDat = {}
 modelOpts = ["KNN", "NBayes", "RandFor", "SVM"]
+thresholdOpts = [round(x,2) for x in list(pd.np.arange(minThreshold,1.0,thresholdInc))]
 modelMissing = []
-## load results
 for modelType in modelOpts:
     subFiles = list(filter(lambda x:modelType in x, chrFiles))
     print(str(len(subFiles)) + " files found for model type " + modelType)
@@ -30,8 +31,6 @@ for modelType in modelOpts:
        allDat[modelType] = utils.loadResults(subFiles[0],"binary", nCT).sort_values(by=["Chr", "Position", "nCpG"])
        ## count
        print(str(len(allDat[modelType])) + " models loaded for model type " + modelType)
-       ## add Density column
-       allDat[modelType]["Density"] = allDat[modelType]['WindowSize']/allDat[modelType]['nCpG']
        ## calc overall accuracy
        sensCols = [col for col in allDat[modelType].columns if col.endswith('sensitivity')]
        allDat[modelType]["MeanAccuracy"] = allDat[modelType][sensCols].mean(1)    
@@ -63,21 +62,22 @@ setattr(granges, "nCpG", allDat[modelOpts[0]]["nCpG"])
 
 ## identify models with sufficinet accuracy
 col = modelOpts + ["BestAccuracy"]
-print("Aggregating models with accuracy > " + str(threshold))
-for ml in col:
-    
-    boolIndex = getattr(granges,ml) > threshold
-    ## count number of models
-    print("Aggregating " + str(sum(boolIndex)) + " " + ml + " models.")
-    if sum(boolIndex) > 0:
+for threshold in thresholdOpts:
+    print("Aggregating models with accuracy > " + str(threshold))
+    for ml in col:
         
-        ## count number of genomic regions - merge into non-overlapping set
-        regions = granges[boolIndex].merge()
-        
-        ## how many models within each region
-        modelOverlaps = regions.coverage(granges[boolIndex])
-        modelOverlaps.Length = modelOverlaps.End - modelOverlaps.Start
-        modelOverlaps.to_csv(str("MergedPredictiveRegionsThreshold" + str(threshold) + "Model" + ml + "Chr" + str(chr) + ".csv"))
-        
+        boolIndex = getattr(granges,ml) > threshold
+        ## count number of models
+        print("Aggregating " + str(sum(boolIndex)) + " " + ml + " models.")
+        if sum(boolIndex) > 0:
+            
+            ## count number of genomic regions - merge into non-overlapping set
+            regions = granges[boolIndex].merge()
+            
+            ## how many models within each region
+            modelOverlaps = regions.coverage(granges[boolIndex])
+            modelOverlaps.Length = modelOverlaps.End - modelOverlaps.Start
+            modelOverlaps.to_csv(str("MergedPredictiveRegionsThreshold" + str(threshold) + "Model" + ml + "Chr" + str(chr) + ".csv"))
+            
 
         
